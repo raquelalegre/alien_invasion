794f0b8e9ae543debfbf9129bd2fa7c33b78fd10
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-06-04 19:45:32 +0100

    Initial dummy main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+This is the entry point of the exercise.
+'''
+
+
+def main():
+    '''
+    Create world reading from file.
+    Populate world with aliens.
+    Loop through time steps and update world's status until world is destroyed
+    or one alien has moved 10K times.
+    '''
+    pass
+
+
+if __name__ == '__main__':
+    main()
