d2a9997f0590394e1034c94643ad56cd3a2ce492
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-06-04 19:51:18 +0100

    Added draft City class.

---
+++ scenario/City.py
@@ -0,0 +1,17 @@
+class City:
+    '''
+    Represents each node in the world graph structure.
+    Node's id is city's name, and the edges are the boundary cities.
+    We need to mark each node as destroyed or not, and know how many aliens
+    there are in a city at a given time (2 or more aliens destroy the city).
+    When the city is destroyed, it is not removed from the world, it's kept
+    so that we don't have to loop through the whole structure to remove all
+    edges pointint to the destroyed city. This way it should be more performant.
+    '''
+    def __init__(self, name):
+        self.name = name
+        self.destroyed = False
+        self.aliens = []
+
+    def process(self):
+        pass
